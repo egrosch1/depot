0166d10f17aff09dc85b38b2d39085b7eb4f8a88
Emily Estrada <egrosch1@my.westga.edu>
2024-02-18 14:39:13 -0500

    E3 Complete

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,5 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
-  
   def add_product(product)
   current_item = line_items.find_by(product_id: product.id)
   if current_item
@@ -9,5 +8,8 @@ class Cart < ApplicationRecord
     current_item = line_items.build(product_id: product.id)
   end
   current_item
+end
+def total_price
+  line_items.sum { |item| item.total_price }
 end
 end
