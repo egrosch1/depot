6ac7e91a776e457e7a77b3aa661f9a06292391fd
Emily Estrada <egrosch1@my.westga.edu>
2024-02-18 21:34:57 -0500

    F2 fixed, F3 complete

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -41,6 +41,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to line_item_url(@line_item)
   end
 
+  test "should create line_item via turbo-stream" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id },
+      as: :turbo_stream
+  end
+
+  assert_response :success
+  assert_match /<tr class="line-item-highlight">/, @response.body
+end
+
   test "should destroy line_item" do
     assert_difference("LineItem.count", -1) do
       delete line_item_url(@line_item)
